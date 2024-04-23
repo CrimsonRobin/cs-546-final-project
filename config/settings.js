d2b745a1a7605ea458c4c017578f2d2edb793f37
Anthony Webster <ajwebster1@gmail.com>
2024-04-23 14:03:07 -0400

    Load Mongo settings from .env

--- config/settings.js
+++ config/settings.js
@@ -1,5 +1,10 @@
 // You will need to change the DB name to match the required DB name in the assignment specs!
+
+import {configDotenv} from "dotenv";
+
+configDotenv({path: "../.env"});
+
 export const mongoConfig = {
-    serverUrl: '',
-    database: ''
+    serverUrl: `https://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`,
+    database: `${process.env.DATABASE_NAME}`
 };
