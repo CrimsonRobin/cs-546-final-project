37989681088ea8fb217d5c1684d857e3467e6ae8
Liam Rust <liamrustm300@gmail.com>
2024-04-24 20:06:22 -0400

    Added static public directory and Handlebars

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 // This file should set up the express server as shown in the lecture code
 
 import express from "express";
+import express_handlebars from "express-handlebars";
 import configRoutes from "./routes/index.js";
 
 const app = express();
 
+app.use("/public", express.static("public"));
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.engine("handlebars", express_handlebars.engine({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 
 configRoutes(app);
 
