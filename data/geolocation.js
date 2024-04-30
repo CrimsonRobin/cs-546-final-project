b45b35f96ff64b3bee6e3d34e01513806026f176
Anthony Webster <ajwebster1@gmail.com>
2024-04-29 23:27:55 -0400

    Add documentation and parse API results.
    Nominatim isn't completely consistent in its API results. This is work to "standardize" the results from API calls to Nominatim into a more predictable format.

--- data/geolocation.js
+++ data/geolocation.js
@@ -13,11 +13,20 @@ const NOMINATIM_API_LOOKUP_ENDPOINT = "/lookup";
 const NOMINATIM_API_SEARCH_ENDPOINT = "/search";
 const NOMINATIM_API_DETAILS_ENDPOINT = "/details";
 
-
 export const OSM_TYPE_NODE = "N";
 export const OSM_TYPE_WAY = "W";
 export const OSM_TYPE_RELATION = "R";
 
+/**
+ * Parses the given OSM type.
+ *
+ * Accepts both the "full names" for the OSM types (such as "node" instead of "N") and
+ * the single-letter abbreviations. Leading and trailing whitespace is ignored.
+ *
+ * @param {*} osmType An OSM type.
+ * @returns {string} The parsed OSM type.
+ * @author Anthony Webster
+ */
 export const parseOsmType = (osmType) =>
 {
     // The OSM type can either be node (N), way (W), or relation (R)
@@ -39,11 +48,46 @@ export const parseOsmType = (osmType) =>
     }
 };
 
+/**
+ * Wraps a Nominatim API call result so that the OSM type and OSM ID are consistently available in
+ * a deterministic location and in a deterministic form.
+ *
+ * Unfortunately, Nominatim isn't completely consistent across all its endpoints, so this function
+ * provides a consistent interface for accessing the most important information (type and ID).
+ *
+ * @param osmType
+ * @param osmId
+ * @param apiResults
+ * @returns {{data, osmId: string, osmType: string}}
+ */
+const createNominatimApiCallResult = (osmType, osmId, apiResults) =>
+{
+    return {
+        // Regardless of the type Nominatim gives us, we'll convert it to what we use internally.
+        osmType: parseOsmType(osmType),
+        // Nominatim likes to give IDs back as numbers, but we use strings.
+        osmId: osmId.toString(),
+        data: apiResults
+    };
+};
+
+/**
+ * Parses an OSM class.
+ * @param {*} osmClass An OSM class.
+ * @returns {string} The parsed OSM class.
+ */
 export const parseOsmClass = (osmClass) =>
 {
     return parseNonEmptyString(osmClass, "OSM class").toLowerCase();
 };
 
+/**
+ * Parses and OSM ID.
+ *
+ * @param {*} osmId The OSM ID.
+ * @returns {string} The parsed OSM ID.
+ * @author Anthony Webster
+ */
 export const parseOsmId = (osmId) =>
 {
     throwIfNotString(osmId, "OSM ID");
@@ -59,6 +103,15 @@ const getNominatimApiUrl = (endpoint) =>
     return new URL(endpoint, NOMINATIM_API_BASE_URL);
 }
 
+/**
+ * Gets the details for the place identified by the given OSM type and OSM ID.
+ *
+ * @param {string} osmType The object's OSM type
+ * @param {string} osmId The object's OSM ID
+ * @param {string | null} osmClass The object's OSM class.
+ * @returns {Promise<{data, osmId: string, osmType: string}>}
+ * @author Anthony Webster
+ */
 export const nominatimDetails = async (osmType, osmId, osmClass = null) =>
 {
     const url = getNominatimApiUrl(NOMINATIM_API_DETAILS_ENDPOINT);
@@ -79,7 +132,7 @@ export const nominatimDetails = async (osmType, osmId, osmClass = null) =>
     }
 
     const {data} = await axios.get(url.toString());
-    return data;
+    return createNominatimApiCallResult(data.osm_type, data.osm_id, data);
 };
 
 export const nominatimLookup = async (osmType, osmId, osmClass = null) =>
@@ -87,20 +140,50 @@ export const nominatimLookup = async (osmType, osmId, osmClass = null) =>
     // TODO
 };
 
+const jsonClone = (obj) => JSON.parse(JSON.stringify(obj));
+
+/**
+ * A result of querying Nominatim's search endpoint.
+ * @typedef {{address: any, osmId: string, displayName: string, importance: number, name: string, osmType: string, category: string, subcategory: string, addressRefersTo: string}} NominatimSearchResult
+ */
+
+/**
+ * Parses a raw search result from Nominatim into a more relevant search result.
+ *
+ * @param searchResult A raw search result from Nominatim.
+ * @returns {NominatimSearchResult} The parsed search result from Nominatim.
+ * @author Anthony Webster
+ */
+const parseSearchResult = (searchResult) =>
+{
+    return {
+        osmType: parseOsmType(searchResult.osm_type),
+        osmId: searchResult.osm_id.toString(),
+        name: searchResult.name,
+        displayName: searchResult.displayName,
+        category: searchResult.category,
+        subcategory: searchResult.type,
+        addressRefersTo: searchResult.addressType,
+        address: jsonClone(searchResult.address),
+        importance: searchResult.importance
+    };
+}
+
 /**
  * Searches the Nominatim database with the given query.
  *
- * @param query
- * @returns {Promise<any>}
+ * @param query The query to search Nominatim.
+ * @returns {Promise<NominatimSearchResult[]>} An array of search results from Nominatim.
  */
 export const nominatimSearch = async (query) =>
 {
     throwIfNotString(query, "Search query");
     const url = getNominatimApiUrl(NOMINATIM_API_SEARCH_ENDPOINT);
-    url.searchParams.format = "json";
     url.searchParams.q = query;
+    url.searchParams.addressdetails = 1;
+    url.searchParams.format = "jsonv2";
     const {data} = await axios.get(url.toString());
-    return data;
+    return data.map(parseSearchResult);
 };
 
 /**
@@ -139,6 +222,4 @@ export const getEnglishName = async (osmType, osmId, osmClass = null) =>
     }
     assertTypeIs(localName, "string", "Local name");
     return localName;
-}
-
-
+};
