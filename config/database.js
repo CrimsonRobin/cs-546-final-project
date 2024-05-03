09ea27c29e072bf50798992cbf38fc17e007d506
Anthony Webster <ajwebster1@gmail.com>
2024-05-03 18:56:45 -0400

    Remove disabilityCategory collection.
    Disability categories will now be stored inline as strings.

--- config/database.js
+++ config/database.js
@@ -1,68 +1,59 @@
-import { Schema, model } from "mongoose";
-import { ObjectId } from "mongodb";
-
-const disabilityCategoriesSchema = new Schema({
-	_id: ObjectId,
-	category: String,
-});
+import {Schema, model} from "mongoose";
+import {ObjectId} from "mongodb";
 
 const usersSchema = new Schema({
-	_id: ObjectId,
-	username: String,
-	hashedPassword: String,
-	createdAt: Date,
-	qualifications: [
-		{
-			category: { type: Schema.Types.ObjectId, ref: "disabilityCategory" },
-		},
-	],
+    _id: ObjectId,
+    username: String,
+    hashedPassword: String,
+    createdAt: Date,
+    qualifications: [String],
 });
 
 const placesSchema = new Schema({
-	_id: ObjectId,
-	name: String,
-	description: String,
-	comments: [
-		{
-			author: { type: String, ref: "users" },
-			content: String,
-			createdAt: Date,
-			likes: Number,
-			dislikes: Number,
-		},
-	],
-	location: {
-		_id: ObjectId,
-		osmId: String,
-		osmType: String,
-		address: String,
-		longitude: Number,
-		latitude: Number,
-	},
-	reviews: [
-		{
-			author: { type: String, ref: "users" },
-			content: String,
-			createdAt: Date,
-			likes: Number,
-			dislikes: Number,
-			categories: [
-				{
-					category: { type: Schema.Types.ObjectId, ref: "disabilityCategory" },
-				},
-			],
-			comments: [
-				{
-					author: { type: String, ref: "users" },
-					createdAt: Date,
-					likes: Number,
-					dislikes: Number,
-				},
-			],
-		},
-	],
+    _id: ObjectId,
+    name: String,
+    description: String,
+    comments: [
+        {
+            author: {type: String, ref: "users"},
+            content: String,
+            createdAt: Date,
+            likes: Number,
+            dislikes: Number,
+        },
+    ],
+    location: {
+        _id: ObjectId,
+        osmId: String,
+        osmType: String,
+        address: String,
+        longitude: Number,
+        latitude: Number,
+    },
+    reviews: [
+        {
+            author: {type: String, ref: "users"},
+            content: String,
+            createdAt: Date,
+            likes: Number,
+            dislikes: Number,
+            categories: [
+                {
+                    category: String,
+                    rating: Number,
+                },
+            ],
+            comments: [
+                {
+                    author: {type: String, ref: "users"},
+                    createdAt: Date,
+                    likes: Number,
+                    dislikes: Number,
+                },
+            ],
+        },
+    ],
 });
 
-export const disabilityCategories = model("disabilityCategories", disabilityCategoriesSchema);
 export const users = model("users", usersSchema);
 export const places = model("places", placesSchema);
