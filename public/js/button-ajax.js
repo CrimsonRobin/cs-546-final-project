6f09a1ba1342573441fd21349e8d27e0cd24ceb3
Liam Rust <liamrustm300@gmail.com>
2024-05-06 17:17:06 -0400

    Have done a Lot of frontend changes
    Things should be somewhat functional now... maybe

--- public/js/button-ajax.js
+++ public/js/button-ajax.js
@@ -1,3 +1,28 @@
 (function () {
-
+    const likeDislikeButtons = document.getElementsByClassName("like-dislike");
+    likeDislikeButtons.map((div) => {
+        const like = div.getElementsByClassName("like")[0];
+        const dislike = div.getElementsByClassName("dislike")[0];
+        const score = div.getElementsByClassName("score")[0];
+        if (like && dislike) {
+            like.addEventListener("click", (event) => {
+                if (like.classList.contains("selected")) {
+                    // undo like
+                } else {
+                    // execute like
+                }
+                // in case the other one was selected
+                dislike.classList.remove("selected");
+            });
+            dislike.addEventListener("click", (event) => {
+                if (like.classList.contains("selected")) {
+                    // undo dislike
+                } else {
+                    // execute dislike
+                }
+                // in case the other one was selected
+                like.classList.remove("selected");
+            });
+        }
+    });
 })();
