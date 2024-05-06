8bb7c46bb71c18c4de2a6bf564089407a917edb2
Christopher Kang <90810005+ChrisKang2003@users.noreply.github.com>
2024-05-05 20:26:59 -0400

    Update spaces

--- data/user.js
+++ data/user.js
@@ -1,19 +1,19 @@
 import {
-  assertTypeIs,
-  isInfinity,
-  isNullOrUndefined,
-  parseCategories,
-  parseDate,
-  parseNonEmptyString,
-  parseObjectId,
-  parseQualifications,
-  roundTo,
-  throwIfNullOrUndefined,
-  nonEmptyStringOrDefault,
+    assertTypeIs,
+    isInfinity,
+    isNullOrUndefined,
+    parseCategories,
+    parseDate,
+    parseNonEmptyString,
+    parseObjectId,
+    parseQualifications,
+    roundTo,
+    throwIfNullOrUndefined,
+    nonEmptyStringOrDefault,
 } from "../helpers.js";
 import {
-  connectToDatabase,
-  closeDatabaseConnection,
+    connectToDatabase,
+    closeDatabaseConnection,
 } from "../config/mongoConnection.js";
 import { User } from "../config/database.js";
 import { parseOsmId, parseOsmType } from "./geolocation.js";
@@ -23,42 +23,49 @@ import { DateTime } from "luxon";
 
 // Create User
 export const parseUserFields = (username, hashedPassword, qualifications) => {
-  // If name and description are not strings or are empty strings, the method should throw.
-  return {
-    username: parseNonEmptyString(username, "Place name"),
-    hashedPassword: parseNonEmptyString(hashedPassword, "Password"),
-    qualifications: parseQualifications(qualifications),
-  };
+    // If name and description are not strings or are empty strings, the method should throw.
+    return {
+        username: parseNonEmptyString(username, "Username"),
+        hashedPassword: parseNonEmptyString(hashedPassword, "Password"),
+        qualifications: parseQualifications(qualifications),
+    };
 };
 
 export const createUser = async (username, hashedPassword, qualifications) => {
-  const parsed = parseUserFields(username, hashedPassword, qualifications);
-  const document = new User({
-    _id: ObjectId,
-    username: username,
-    hashedPassword: hashedPassword,
-    createdAt: DateTime.now().toBSON(),
-    qualifications: qualifications,
-  });
+    const parsed = parseUserFields(username, hashedPassword, qualifications);
+    const document = new User({
+        _id: ObjectId,
+        username: username,
+        hashedPassword: hashedPassword,
+        createdAt: DateTime.now().toBSON(),
+        qualifications: qualifications,
+    });
 
-  await document.save();
+    await document.save();
+    return document;
 };
 
 // Get User
 export const getUser = async (userId) => {
-  userId = parseObjectId(userId, "User id");
-  const collection = await User();
-  const result = await collection.findOne({
-    _id: ObjectId.createFromHexString(userId),
-  });
-  if (!result) {
-    throw new Error(`Failed to find product with id ${userId}`);
-  }
-  result._id = result._id.toString();
-  return result;
+    userId = parseObjectId(userId, "User id");
+    const collection = await User();
+    const result = await collection.findOne({
+        _id: ObjectId.createFromHexString(userId),
+    });
+    if (!result) {
+        throw new Error(`Failed to find user with id ${userId}`);
+    }
+    result._id = result._id.toString();
+    return result;
 };
 // Get All Users
-export const getUsers = () => {};
+export const getUsers = async () => {
+    const userCollection = await User();
+    let userList = await userCollection.find({}).toArray();
+    return userList;
+};
+// Update User
+export const updateUser = async () => {};
 // Get Average Rating
 export const getAvgRating = () => {};
 // Get amount of reviews
