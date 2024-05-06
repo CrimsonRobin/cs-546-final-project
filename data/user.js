a1ee1169f3f1721df4432db0b89228ac47869bd8
Christopher Kang <90810005+ChrisKang2003@users.noreply.github.com>
2024-05-06 18:13:02 -0400

    Small change from 12 to variable

--- data/user.js
+++ data/user.js
@@ -35,7 +35,7 @@ export const createUser = async (firstname, lastname, username, password, qualif
         firstname: firstname,
         lastname: lastname,
         username: username,
-        hashedPassword: await bcrypt.hash(password, 12),
+        hashedPassword: await bcrypt.hash(password, BCRYPT_SALT_ROUNDS),
         createdAt: DateTime.now().toBSON(),
         qualifications: removeDuplicates(qualifications),
     });
